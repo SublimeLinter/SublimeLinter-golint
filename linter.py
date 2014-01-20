6edeedc975bf2f10fade894e8563ba4ba4423ccf
SirReal <jon@surrell.es>
2014-01-20 02:09:23 +0100

    Cleanup linter.py unused vars

--- linter.py
+++ linter.py
@@ -19,17 +19,6 @@ class Golint(Linter):
 
     syntax = 'go'
     cmd = 'golint'
-    executable = None
-    # 001.go:16:19: should drop = 0 from declaration of var result; it is the zero value
     regex = r'^.+:(?P<line>\d+):(?P<col>\d+):\s+(?P<message>.+)$'
-
-    multiline = False
-    line_col_base = (1, 1)
     tempfile_suffix = 'go'
-    error_stream = util.STREAM_BOTH
-    selectors = {}
-    word_re = None
-    defaults = {}
-    inline_settings = None
-    inline_overrides = None
-    comment_re = None
+    error_stream = util.STREAM_STDOUT
