418d5eef8a4374cf9948a27bb7506a43c966394c
SirReal <jon.surrell@gmail.com>
2014-01-21 15:59:11 +0100

    Removed line end $ from regex

--- linter.py
+++ linter.py
@@ -19,6 +19,6 @@ class Golint(Linter):
 
     syntax = 'go'
     cmd = 'golint'
-    regex = r'^.+:(?P<line>\d+):(?P<col>\d+):\s+(?P<message>.+)$'
+    regex = r'^.+:(?P<line>\d+):(?P<col>\d+):\s+(?P<message>.+)'
     tempfile_suffix = 'go'
     error_stream = util.STREAM_STDOUT
