25dffb918a1db2382989ae4b3805a119766e7927
Koen Lageveen <koen.lageveen@me.com>
2018-04-03 20:18:45 +0200

    remove gopaths stuff

--- linter.py
+++ linter.py
@@ -1,10 +1,4 @@
-#
-# Written by Jon Surrell and Jeremy Jay
-# Copyright (c) 2014 Jon Surrell
-#
-
-import os
-from SublimeLinter.lint import util, persist, Linter, WARNING
+from SublimeLinter.lint import util, Linter, WARNING
 
 
 class Golint(Linter):
@@ -16,41 +10,3 @@ class Golint(Linter):
     defaults = {
         'selector': 'source.go'
     }
-
-    def find_gopaths(self):
-        """Search for potential GOPATHs."""
-        # collect existing Go path info
-        goroot = set(os.path.normpath(s) for s in os.environ.get('GOROOT', '').split(os.pathsep))
-        gopath = set(os.path.normpath(s) for s in os.environ.get('GOPATH', '').split(os.pathsep))
-        if '.' in gopath:
-            gopath.remove('.')
-        gopath = list(gopath)
-
-        # search for potential GOPATHs upstream from filename
-        # (reversed to ensure deepest path is first searched)
-        dirparts = os.path.dirname(self.filename).split(os.sep)
-        for i in range(len(dirparts) - 1, 1, -1):
-            if dirparts[i].lower() != "src":
-                continue
-            p = os.path.normpath(os.sep.join(dirparts[:i]))
-            if p not in goroot and p not in gopath:
-                gopath.append(p)
-
-        persist.debug("{}: {} {}".format(
-            self.name,
-            os.path.basename(self.filename or '<unsaved>'),
-            "guessed GOPATH=" + os.pathsep.join(gopath)))
-
-        return os.pathsep.join(gopath)
-
-    def run(self, cmd, code):
-        """Transparently add potential GOPATHs before running."""
-        self.env = {'GOPATH': self.find_gopaths()}
-
-        # copy debug output from Linter.run()
-        persist.debug('{}: {} {}'.format(
-            self.name,
-            os.path.basename(self.filename or '<unsaved>'),
-            cmd or '<builtin>'))
-
-        return self.tmpfile(cmd, code, suffix=self.get_tempfile_suffix())
